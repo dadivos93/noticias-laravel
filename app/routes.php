98696513b2b24c7d8ea25f99766e50b57b9ef713
David Garcia <davidj.garcastillo@gmail.com>
2014-11-24 17:25:56 -0600

    Route componente de trabajo de rutas

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,37 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::get('usuarios', function()
+{
+    return "Hola Mundo";
+});
+
+Route::post('usuarios', function()
+{
+    return View::make('hello');
+});
+
+Route::get('usuario/{nombre?}/{edad?}', function($nombre=null, $edad=null)
+{
+    if(!$nombre) {
+        return "Hola Mundo";
+    } else {
+        return "Hola " . $nombre . " tu edad es " . $edad;
+    }
+});
+
+Route::match(array('GET', 'POST'), '/', function()
+{
+    return 'Hello World';
+});
+
+Route::any('foo', function()
+{
+    return 'Hello World';
+});
+
+Route::get('foo', array('https', function()
+{
+    return 'Must be over HTTPS';
+}));
